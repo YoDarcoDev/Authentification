f2fae733b5e5ed4618884441fc210f684c191996
Yoann Bettinelli <yoann.bettinelli@gmail.com>
2020-10-14 15:01:22 +0200

    app.js done + fix css)

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const btnConnexion = document.querySelector('.btn-connexion');
 const btnDeco = document.querySelector('.btn-deco');
 
 const formInscription = document.querySelector('.form-inscription');
-const emailInscription = document.querySelector('.')
+const emailInscription = document.querySelector('.email-inscription');
+const mdpInscription = document.querySelector('.mdp-inscription');
 
 const formConnexion = document.querySelector('.form-connexion');
 
+
+// BTN INSCRIPTION
 
 btnInscription.addEventListener('click', () => {
     
@@ -19,6 +22,8 @@ btnInscription.addEventListener('click', () => {
 
 
 
+// BTN CONNEXION
+
 btnConnexion.addEventListener('click', () => {
     
     if (formInscription.classList.contains('apparition')) {
@@ -30,10 +35,86 @@ btnConnexion.addEventListener('click', () => {
 
 
 
+// BTN DECONNEXION
+
+btnDeco.addEventListener('click', (e) => {
+
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log("Déconnecté")
+    })
+})
+
+
+
+
+// FORM INSCRIPTION
+
 formInscription.addEventListener('submit', (e) => {
    
-    e.preventDefault();
+    e.preventDefault();         // envoie des données vers firebase, ne pas recharger la page
 
     const mailValeur = emailInscription.value;
     const mdpInscriptionValeur = mdpInscription.value
-})                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+
+    // console.log(mailValeur);
+    // console.log(mdpInscriptionValeur);
+
+    auth.createUserWithEmailAndPassword(mailValeur, mdpInscriptionValeur)
+    .then(credentials => {
+        console.log(credentials);
+
+        formInscription.reset();        // Clean les inputs
+        formInscription.classList.toggle('apparition');
+    })
+})     
+
+
+
+
+// FORM CONNEXION
+
+formConnexion.addEventListener('submit', (e) => {
+   
+    e.preventDefault();
+
+    const mailValeur = emailConnexion.value;
+    const mdpConnexionValeur = mdpConnexion.value
+
+    // console.log(mailValeur);
+    // console.log(mdpConnexionValeur);
+
+    auth.signInWithEmailAndPassword(mailValeur, mdpConnexionValeur)
+    .then(credentials => {
+
+        console.log(credentials);
+        console.log("Connecté", credentials.user);
+
+        formConnexion.reset();
+        formConnexion.classList.toggle('apparition');
+    })
+})  
+
+
+
+// GERER LE CONTENU
+
+auth.onAuthStateChanged(utilisateur => {                // Voir si un user est connecté ou pas
+
+    const h1 = document.querySelector('h1');
+    const info = document.querySelector('.info');
+
+    if (utilisateur) {
+        info.innerText = "Voici le contenu privé";
+        h1.innerText = "Vous voilà de retour !";
+    }
+
+    else {
+        console.log("Utilisateur s'est déconnecté");
+        info.innerText = "Contenu public.";
+        h1.innerText = "Bienvenue, inscrivez-vous ou connectez-vous";
+    }
+})
+
+
+
